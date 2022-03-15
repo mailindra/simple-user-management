fba8688b3a53d80f192662d3f0a65d4c34da6c2a
Ronny Mailindra <ronny@g2academy.co>
2022-03-15 12:16:28 +0700

    Initial commit to setup project. Include all files generated in the spring intializer Adding test and logging

---
+++ src/main/java/com/mailindra/demo/simpleUserManagement/SimpleUserManagementApplication.java
@@ -0,0 +1,13 @@
+package com.mailindra.demo.simpleUserManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleUserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleUserManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mailindra/demo/simpleUserManagement/SimpleUserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mailindra.demo.simpleUserManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleUserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
